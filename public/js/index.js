3716c28113689cd44fd66bc10616496f1082e0c3
na08skt <sakata.na08@gmail.com>
2022-08-03 11:51:50 +0000

    save じゃんけんシステム製作中

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,69 @@
+// じゃんけん
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。')
+let js_hand = getJShand();
+let judge = winLose(user_hand, js_hand);
+
+if(user_hand != "グー"　|| user_hand != "パー"　|| user_hand != "チョキ"){
+alert('グー・チョキ・パーのいずれかを入力してください');
+} else  alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+
+
+
+function getJShand(){
+  let js_hand_num = Math.floor( Math.random() * 3 );
+  let hand_name;
+
+  if(js_hand_num == 0){
+    hand_name = "グー";
+  } else if(js_hand_num == 1){
+    hand_name = "チョキ";
+  } else if(js_hand_num == 2){
+    hand_name = "パー";
+  }
+
+  return hand_name;
+}
+
+function winLose(user, js){
+  let winLoseStr;
+
+  if(user == "グー"){
+    if(js == "グー"){
+      winLoseStr = "あいこ";
+    } else if(js == "チョキ"){
+      winLoseStr = "勝ち";
+    } else if(js == "パー"){
+      winLoseStr = "負け";
+    }
+  } else if(user == "チョキ"){
+    if(js == "グー"){
+      winLoseStr = "負け";
+    } else if(js == "チョキ"){
+      winLoseStr = "あいこ";
+    } else if(js == "パー"){
+      winLoseStr = "勝ち";
+    }
+  } else if(user == "パー"){
+    if(js == "グー"){
+      winLoseStr = "勝ち";
+    } else if(js == "チョキ"){
+      winLoseStr = "負け";
+    } else if(js == "パー"){
+      winLoseStr = "あいこ";
+    }
+  }
+
+  return winLoseStr;
+}
+
+// let promptStr = prompt('好きな文字を入力')
+
+// alert(promptStr)
+
 // while& do whileのための定義
-let max = 100;
-let num = 1;
-let count = 0;
+// let max = 100;
+// let num = 1;
+// let count = 0;
 
 // while(num<max){
 //   num = num*2
@@ -11,12 +73,12 @@ let count = 0;
 // alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
 
 // do while
-do{
-  num *= 3;
-  count += 1;
-}while(num < max);
+// do{
+//   num *= 3;
+//   count += 1;
+// }while(num < max);
 
-alert("numがmaxを越えるまでに必要だった回数は"+count+"回です");
+// alert("numがmaxを越えるまでに必要だった回数は"+count+"回です");
 
 // for 文のための定義
 // let i;
