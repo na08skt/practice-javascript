862195ee3d84f6011e3d4be6a03caf1000c591ea
na08skt <sakata.na08@gmail.com>
2022-08-03 12:11:12 +0000

    clear じゃんけんシステム

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,15 @@
 // じゃんけん
-let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。')
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
 let js_hand = getJShand();
 let judge = winLose(user_hand, js_hand);
 
-if(user_hand != "グー"　|| user_hand != "パー"　|| user_hand != "チョキ"){
-alert('グー・チョキ・パーのいずれかを入力してください');
-} else  alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
-
-
+if(user_hand == "グー" || user_hand == "パー" || user_hand == "チョキ"){
+  alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+} else if(user_hand == null) {
+  alert('またチャレンジしてね');
+} else {
+  let user_hand = prompt('グー・チョキ・パーのいずれかを入力してください');
+}
 
 function getJShand(){
   let js_hand_num = Math.floor( Math.random() * 3 );
