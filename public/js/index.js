6cc57fce34f8056ac60de441d25f7424e4b6739d
na08skt <sakata.na08@gmail.com>
2022-08-03 09:52:06 +0000

    clear do while 今回はすぐに表示が変更された

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,22 @@
-// while分のための定義
+// while& do whileのための定義
 let max = 100;
 let num = 1;
 let count = 0;
 
-while(num<max){
-  num = num*2
+// while(num<max){
+//   num = num*2
 
-  count += 1
-}
-alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+//   count += 1
+// }
+// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+// do while
+do{
+  num *= 3;
+  count += 1;
+}while(num < max);
+
+alert("numがmaxを越えるまでに必要だった回数は"+count+"回です");
 
 // for 文のための定義
 // let i;
