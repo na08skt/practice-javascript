7367e2f338c437062c633e208179839c9e2cd8fd
na08skt <sakata.na08@gmail.com>
2022-08-03 09:42:41 +0000

    clear while文　変更が表示変更されるときされない時がある

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,21 @@
-let i;
-let num = 0;
+// while分のための定義
+let max = 100;
+let num = 1;
+let count = 0;
 
-for(i = 1; i < 11; i++){
-    num = num + i;
+while(num<max){
+  num = num*2
+
+  count += 1
 }
+alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
 
-alert('1から１０まで足した数は' + num + 'です');
+// for 文のための定義
+// let i;
+// let num = 0;
+
+// for(i = 1; i < 11; i++){
+//     num = num + i;
+// }
+
+// alert('1から１０まで足した数は' + num + 'です');
